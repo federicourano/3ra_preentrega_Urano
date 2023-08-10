4813622e6f3ef8ae1da67be40850761de45fd3f6
Federico Urano <federicourano2013@gmail.com>
2023-08-10 12:12:30 -0300

    Checkpoint para agregar un nuevo model

--- miaplicacion/models.py
+++ miaplicacion/models.py
@@ -25,3 +25,10 @@ class Staff(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
+    
+class Sagas(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidadDeLibros = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre}"
